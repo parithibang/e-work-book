e8b11c6ec2b29c62dd5d4ee978886fa52752d136
Aravind Kumar G <aravind.kumar@emishealth.com>
2020-01-07 19:02:23 +0530

    Basic Template

--- controllers/default.go
+++ controllers/default.go
@@ -4,12 +4,24 @@ import (
 	"github.com/astaxie/beego"
 )
 
+// MainController doc
 type MainController struct {
 	beego.Controller
 }
 
+// ActiveContent is the common template
+func (c *MainController) ActiveContent(view string) {
+	c.Layout = "basic-layout.tpl"
+	c.LayoutSections = make(map[string]string)
+	c.LayoutSections["Header"] = "header.tpl"
+	c.LayoutSections["Sidebar"] = "sidebar.tpl"
+	c.LayoutSections["Footer"] = "footer.tpl"
+	c.TplName = view + ".tpl"
+}
+
+// Get doc
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+	c.ActiveContent("index")
+	c.Data["Website"] = "eWorkBook"
+	c.Data["Email"] = "eworkbook@gmail.com"
 }
--- routers/router.go
+++ routers/router.go
@@ -2,9 +2,11 @@ package routers
 
 import (
 	"e-work-book/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/home", &controllers.MainController{})
 }
